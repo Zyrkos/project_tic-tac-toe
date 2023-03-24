c0e3bb37fbdbbd46878e5d6c859b6bf342e2fdcd
Zyrkos <pedrocastro1995@hotmail.com>
2023-03-24 19:44:09 +0000

    .

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const startGameBtn = document.getElementById("start-game-btn");
-const playerChoiceX = document.getElementById("x-choice");
-const playerChoiceO = document.getElementById("o-choice");
-const gameBoard = document.getElementById("game-board");
-const gameBoardField = document.getElementsByClassName("game-board-field");
+const startGameBtn = document.getElementById('start-game-btn');
+const playerChoiceX = document.getElementById('x-choice');
+const playerChoiceO = document.getElementById('o-choice');
+const gameBoard = document.getElementById('game-board');
+const gameBoardField = document.getElementsByClassName('game-board-field');
 
 /* function createPlayer(choice, score) {
   let score = 0;
@@ -24,13 +24,13 @@ const gameBoardField = document.getElementsByClassName("game-board-field");
 } */
 
 function createGameBoard() {
-  const gameBoardDiv = document.createElement("div");
-  gameBoardDiv.classList.add("game-board");
-  gameBoardDiv.id = "game-board";
+  const gameBoardDiv = document.createElement('div');
+  gameBoardDiv.classList.add('game-board');
+  gameBoardDiv.id = 'game-board';
 
   for (let i = 0; i < 9; i++) {
-    const gameBoardFieldDiv = document.createElement("div");
-    gameBoardFieldDiv.classList.add("game-board-field");
+    const gameBoardFieldDiv = document.createElement('div');
+    gameBoardFieldDiv.classList.add('game-board-field');
     gameBoardFieldDiv.id = `game-board-field-${i}`;
     gameBoardDiv.appendChild(gameBoardFieldDiv);
   }
@@ -40,11 +40,11 @@ function createGameBoard() {
 
 function gameFlow() {
   function playerTurn() {
-    const gameBoardField = document.getElementsByClassName("game-board-field");
+    const gameBoardField = document.getElementsByClassName('game-board-field');
     for (let i = 0; i < gameBoardField.length; i++) {
-      gameBoardField[i].addEventListener("click", function () {
-        this.textContent = "X";
-        this.id += "-X";
+      gameBoardField[i].addEventListener('click', function () {
+        this.textContent = 'X';
+        this.id += '-X';
       });
     }
   }
@@ -55,3 +55,5 @@ function gameFlow() {
 
 const game = gameFlow();
 game.playerTurn();
+
+
