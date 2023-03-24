53ffce018855f3c41ee1f49fcc9cfb23f6470f6a
Zyrkos <pedrocastro1995@hotmail.com>
2023-03-24 19:15:24 +0000

    .

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 const playerChoiceX = document.getElementById("x-choice");
 const playerChoiceO = document.getElementById("o-choice");
-const gameBoard = document.getElementById("game-board")
-const gameBoardField = document.getElementById("game-board-field")
+const gameBoard = document.getElementById("game-board");
+const gameBoardField = document.getElementsByClassName("game-board-field");
 
 
-
-
-
-
-
-function createPlayer(choice) {
+/* function createPlayer(choice, score) {
   let score = 0;
+  let choice = "X";
 
-  function getChoice() {
+  function getChoice(choice) {
+    
     return choice;
   }
 
@@ -24,6 +21,46 @@ function createPlayer(choice) {
     getChoice,
     getScore,
   };
+} */
+
+  function createGameBoard() {
+
+
+    
+    const gameBoardDiv = document.createElement('div');
+    gameBoardDiv.classList.add('game-board');
+    gameBoardDiv.id = 'game-board';
+  
+    for (let i = 0; i < 9; i++) {
+      const gameBoardFieldDiv = document.createElement('div');
+      gameBoardFieldDiv.classList.add('game-board-field');
+      gameBoardFieldDiv.id = `game-board-field-${i}`;
+      gameBoardDiv.appendChild(gameBoardFieldDiv);
+    }
+  
+    return gameBoardDiv;
+  }
+
+
+ function gameFlow() {
+/* playerTurn will be added here */
+
+  }
+
+    
+
+
+
+
+
+
+function playerTurn() { 
+  for (let i = 0; i < gameBoardField.length; i++) {
+    gameBoardField[i].addEventListener('click', function() {
+    this.textContent = "X"; // we use "this" to add an X to the specific div we're clicking
+    this.id += "-X"; // we then concatenate "-X" to the div's ID
+    });
+  }
 }
 
-
+playerTurn();
