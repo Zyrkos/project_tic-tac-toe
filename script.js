6ab125566e3c2de1867cf4b9dc6bb10f6a533d94
Pedro Castro <106441980+Zyrkos@users.noreply.github.com>
2023-03-26 22:32:41 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
-const gameBoard = (() => {
+/* const playerOne = document.getElementById("x-choice");
+const playerTwo = document.getElementById("o-choice");
+const gameBoard = document.getElementById("game-board");
+const startGameBtn = document.getElementById("start-game-btn"); */
+
+/* function createPlayer(symbol, score) {
+  let playerOne;
+  let playerTwo;
+  let symbol;
+
+  function getSymbol(symbol) {
+    playerOne.addEventListener("click", function () {
+      playerOneSymbol = "x";
+    });
+    playerTwo.addEventListener("click", function () {
+      playerTwoSymbol = "o";
+    });
+
+    return symbol;
+  }
+
+  return {
+    getSymbol,
+  };
+} */
+
+const gameBoard = (function () {
   const boardFields = ["", "", "", "", "", "", "", "", ""];
+
+  const button = document.getElementById("start-game-btn");
+  button.addEventListener("click", function () {
+    gameBoard.createBoard();
+    button.removeEventListener("click", arguments.callee); //to stop the start game from deploying more grids after one click
+  });
 
   const createBoard = () => {
     const board = document.createElement("div");
-    board.classList.add("game-board");
+    board.classList.add("board");
 
     for (let i = 0; i < boardFields.length; i++) {
       const field = document.createElement("div");
@@ -11,30 +43,35 @@ const gameBoard = (() => {
       field.setAttribute("data-index", i);
       field.addEventListener("click", (e) => {
         const clickedIndex = e.target.getAttribute("data-index");
-        boardFields[clickedIndex] = "X"; // or "O", depending on the player
-        field.textContent = boardFields[clickedIndex];
+        boardFiels[clickedIndex] = "X";
       });
+
       board.appendChild(field);
     }
-    document.getElementById("game").appendChild(board);
-  };
 
-  return {
-    createBoard,
+    document.getElementById("game").appendChild(board);
+    
   };
+  return {
+    createBoard
+  }
 })();
 
-const gameFlow = () => {
-  const startGame = () => {
-    const button = document.getElementById("start-game-btn");
-    button.addEventListener("click", () => {
-      gameBoard.createBoard();
-    });
-  };
 
-  return {
-    startGame
+
+
+
+function gameFlow() {
+  function playerTurn() {
+    const gameBoardField = document.getElementsByClassName("game-board-field");
+    for (let i = 0; i < gameBoardField.length; i++) {
+      gameBoardField[i].addEventListener("click", function () {
+        this.textContent = "X";
+        this.id += "-X";
+      });
+    }
   }
-};
-
-const startGame = gameFlow().startGame;
+  return {
+    playerTurn,
+  };
+}
