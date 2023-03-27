8bafa60d07257dad05ad9380185b84d99a640d35
Zyrkos <pedrocastro1995@hotmail.com>
2023-03-27 17:40:29 +0100

    .

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+const player = () => {
+  const getCurrentPlayer = () => {
+    const btnX = document.getElementById("x-choice");
+    const btnO = document.getElementById("o-choice");
+  };
+};
+
 const gameBoard = (function () {
   const boardFields = ["", "", "", "", "", "", "", "", ""];
-
   const button = document.getElementById("start-game-btn");
   button.addEventListener("click", function () {
     gameBoard.createBoard();
@@ -17,7 +23,17 @@ const gameBoard = (function () {
       field.setAttribute("data-index", i);
       field.addEventListener("click", (e) => {
         const clickedIndex = e.target.getAttribute("data-index");
-        boardFields[clickedIndex] = "";
+        if (boardFields[clickedIndex] === "") {
+          const currentPlayer = game.getCurrentPlayer();
+          e.target.textContent = currentPlayer.symbol;
+          boardFields[clickedIndex] = currentPlayer.symbol;
+          const winner = game.checkForWinner();
+          if (winner !== null) {
+            game.endGame(winner);
+          } else {
+            game.changeTurn();
+          }
+        }
       });
 
       board.appendChild(field);
@@ -31,7 +47,15 @@ const gameBoard = (function () {
   };
 })();
 
-const playerTurn = (index) => {
+const gameFlow = () => {
+  const changeTurn = () => {};
+
+  const checkForWinner = () => {};
+
+  const endGame = () => {};
+};
+
+/* const playerTurn = (index) => {
   const boardFields = ["", "", "", "", "", "", "", "", ""];
   const field = document.querySelector(`.field[data-index="${index}"]`);
   if (boardFields[index] === "") {
@@ -39,3 +63,4 @@ const playerTurn = (index) => {
     boardFields[index] = "X";
   }
 };
+ */
